25a19fdc91ec5ad0017e61da66fd170632716c6a
Luca Paganotti <lucapaganotti@users.noreply.github.com>
2019-06-27 12:30:13 +0200

    Update config_iris.php

--- html/common/config_iris.php
+++ html/common/config_iris.php
@@ -8,14 +8,14 @@ $root_dir_cgi = '/cgi-bin';
 $root_dir_script = '/common/webgis_central.php';
 //$proxies_http = 'proxy.arpa.piemonte.it';
 //$proxies_port = '3128';
-$proxies_http = 'meteo:%meteo2010@proxy2.arpa.local';
+$proxies_http = '<user>:<password>@proxy2.arpa.local';
 $proxies_port = '8080';
 
-$conn_string = "host=localhost port=5432 dbname=iris_base user=webgis password=webgis$2013%";
-$conn_string_edit = "host=localhost port=5432 dbname=iris_base user=radar password=dirac0";
-$conn_string_admin = "host=localhost port=5432 dbname=iris_base user=postgres password=p0stgr3S";
+$conn_string = "host=localhost port=5432 dbname=iris_base user=<user> password=<password>";
+$conn_string_edit = "host=localhost port=5432 dbname=iris_base user=<user> password=<password>";
+$conn_string_admin = "host=localhost port=5432 dbname=iris_base user=<user> password=<password>";
 
-$dns = "postgresql://radar:dirac0@localhost:5432/iris_base"; //permessi di scrittura su alcune tabelle config
+$dns = "postgresql://<user>:<password>0@localhost:5432/iris_base"; //permessi di scrittura su alcune tabelle config
 
 ?>
 
